9d70cdf9e2f184f3c2d797dcb3c9f3abb762f880
Basudev18 <das1basudev1995@gmail.com>
2023-03-22 09:41:25 +0530

    All Codes updated
    Signed-off-by: Basudev18 <das1basudev1995@gmail.com>

---
+++ usb/rd.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<unistd.h>
+int main()
+{
+	int fd;
+        char buf[20];
+	fd=open("/home/vipra/workspace/usb/hello.txt",O_RDWR|O_CREAT);
+	if(fd==-1)
+	{
+		printf("Failed to create file");
+	}
+	else
+	{
+		printf("File created successfully");
+	}
+	
+        read(fd,buf,20);
+	printf("%s",buf);
+
+}
